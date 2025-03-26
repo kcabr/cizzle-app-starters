32e368341b438d15a93a2b920ad52562d3730ac2
KC Abramson <kc.abramson@gmail.com>
2025-03-26 11:12:11 -0400

    Netlify 500 error. Switched to Vercel

--- templates/tanstack-start-clerk-supabase-shadcn/app.config.ts
+++ templates/tanstack-start-clerk-supabase-shadcn/app.config.ts
@@ -3,7 +3,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
-    preset: "netlify",
+    preset: "vercel",
   },
   tsr: {
     appDirectory: "src",
