acc6e29d0204cdf83599ca4a3a618b93ad604d8d
KC Abramson <kc.abramson@gmail.com>
2025-03-28 03:14:14 -0400

    Refine enterprise/B2B starter created

--- templates/tanstack-start-clerk-supabase.OLD/src/utils/createEmotionCache.ts
+++
@@ -1,7 +0,0 @@
-import createCache from "@emotion/cache";
-
-// On the client side, Create a theme instance.
-export function createEmotionCache() {
-  return null;
-  createCache({ key: "css" });
-}
